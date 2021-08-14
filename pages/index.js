1fe2bfe3c2379b7e97c27507aa4cdc26cde5ccfe
Denis Santiago <denissanthiago@gmail.com>
2021-08-14 00:22:42 -0500

    [Feat] Change text name

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <Title>My page</Title>
+        <Title>My Denis</Title>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
